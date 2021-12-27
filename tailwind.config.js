99af56439bb5ce874be0bca7ad4273273d3d95eb
maftuhlutfi <playmakernyomlo@gmail.com>
2021-12-27 13:31:12 +0700

    add border radius

--- tailwind.config.js
+++ tailwind.config.js
@@ -22,6 +22,9 @@ module.exports = {
         'linear-primary': 'linear-gradient(315deg, #91A2FE -0.62%, #C0CBF9 111.88%)',
         'linear-red': 'linear-gradient(315deg, #E06C6C -0.62%, #FFA3A3 111.88%)',
         'linear-green': 'linear-gradient(315deg, #39BF67 -0.62%, #9CFFA0 111.88%)'
+      },
+      borderRadius: {
+        '4xl': '32px'
       }
     },
   },
