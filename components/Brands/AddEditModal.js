052e8d670081860ddc4e2ea49f919632c4108029
maftuhlutfi <playmakernyomlo@gmail.com>
2022-01-04 01:59:12 +0700

    image input for brand image

--- components/Brands/AddEditModal.js
+++ components/Brands/AddEditModal.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import Button from "../shared/Button";
+import ImageInput from "../shared/Input/ImageInput";
 import TextArea from "../shared/Input/TextArea";
 import TextField from "../shared/Input/TextField";
 import Modal from "../shared/Modal";
@@ -14,14 +15,25 @@ const AddEditModal = ({ show, brandData, onClose }) => {
         instagram: '',
         shopee: '',
         tokopedia: '',
-        image: ''
+        image: null
     }
 
     const [input, setInput] = useState(initialInput)
     const { name, description, location, website, instagram, shopee, tokopedia, image } = input
 
+    const [imageData, setImageData] = useState(null)
+
     const handleChange = e => {
-        const { name, value } = e.target
+        const { name, value, files } = e.target
+
+        if (files) {
+            setInput(prev => ({
+                ...prev,
+                image: files[0]
+            }))
+            setImageData(URL.createObjectURL(files[0]))
+            return
+        }
 
         setInput(prev => ({
             ...prev,
@@ -29,29 +41,36 @@ const AddEditModal = ({ show, brandData, onClose }) => {
         }))
     }
 
+    const handleSubmit = e => {
+        e.preventDefault()
+    }
+
     useEffect(() => {
         if (show && brandData) {
             setInput(brandData)
+            setImageData(brandData.image)
         } else {
             setInput(initialInput)
+            setImageData(null)
         }
     }, [show])
 
     return (
         <Modal show={show} onClose={onClose}>
             <ModalTitle title='Add/Edit New Brand' />
-            <form className="flex flex-col w-full mb-10">
-                <TextField placeholder='Name' className='bg-purple-light mb-6' name='name' value={name} onChange={handleChange} />
-                <TextArea placeholder='Description' className='bg-purple-light mb-6 h-32' name='description' value={description} onChange={handleChange} />
-                <TextField placeholder='Location' className='bg-purple-light mb-6' name='location' value={location} onChange={handleChange} />
-                <TextField placeholder='Website' className='bg-purple-light mb-6' name='website' value={website} onChange={handleChange} />
-                <div className="grid grid-cols-3 gap-6">
-                    <TextField placeholder='Instagram' className='bg-purple-light' name='instagram' value={instagram} onChange={handleChange} />
-                    <TextField placeholder='Shopee' className='bg-purple-light' name='shopee' value={shopee} onChange={handleChange} />
-                    <TextField placeholder='Tokopedia' className='bg-purple-light' name='tokopedia' value={tokopedia} onChange={handleChange} />
+            <form className="flex flex-col w-full" onSubmit={handleSubmit}>
+                <TextField placeholder='Name' className='bg-purple-light mb-6' name='name' value={name} onChange={handleChange} required />
+                <TextArea placeholder='Description' className='bg-purple-light mb-6 h-32' name='description' value={description} onChange={handleChange} required />
+                <TextField placeholder='Location' className='bg-purple-light mb-6' name='location' value={location} onChange={handleChange} required />
+                <div className="grid grid-cols-3 gap-6 mb-10">
+                    <TextField placeholder='Website' className='bg-purple-light' name='website' value={website} onChange={handleChange} required />
+                    <TextField placeholder='Instagram' className='bg-purple-light' name='instagram' value={instagram} onChange={handleChange} required />
+                    <ImageInput id='brand-image' name='image' className='h-full w-full row-span-2' image={imageData} onChange={handleChange} required />
+                    <TextField placeholder='Shopee' className='bg-purple-light' name='shopee' value={shopee} onChange={handleChange} required />
+                    <TextField placeholder='Tokopedia' className='bg-purple-light' name='tokopedia' value={tokopedia} onChange={handleChange} required />
                 </div>
+                <Button className={'relative mx-auto w-full'}>Save</Button>
             </form>
-            <Button className={'relative mx-auto w-full'}>Save</Button>
         </Modal>
     );
 }
--- components/shared/Modal/index.js
+++ components/shared/Modal/index.js
@@ -6,8 +6,8 @@ const Modal = ({ children, onClose, show }) => {
     useOnClickOutside(ref, onClose)
 
     return (
-        <div className={`min-h-screen w-screen bg-[#858FB4] bg-opacity-80 ${show ? 'flex' : 'hidden'} items-center justify-center absolute inset-0 z-40`}>
-            <div ref={ref} className="w-[512px] bg-white rounded-3xl p-10 pt-12 relative">
+        <div className={`min-h-screen overflow-y-auto py-10 w-screen bg-[#858FB4] bg-opacity-80 ${show ? 'block' : 'hidden'} items-center justify-center absolute inset-0 z-40`}>
+            <div ref={ref} className="w-[512px] bg-white rounded-3xl p-10 pt-12 relative mx-auto">
                 <div onClick={onClose} className="absolute top-6 right-6 text-xl cursor-pointer">&#10006;</div>
                 {children}
             </div>
